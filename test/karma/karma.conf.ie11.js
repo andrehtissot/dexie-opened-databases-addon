b15e08316654289fe6b53e385635807795822807
André Augusto Tissot <andrehtissot@gmail.com>
2019-01-05 15:20:29 +0100

    Improvements reverted

--- test/karma/karma.conf.ie11.js
+++ test/karma/karma.conf.ie11.js
@@ -3,7 +3,7 @@ const configurations = require('./karma.common')
 module.exports = (config) => {
     configurations.browsers = ['IE']
     configurations.plugins.push('karma-ie-launcher')
-    configurations.files.push('dist/DexieOpenedDatabasesAddon.js')
-    configurations.files.push('test/tmp/tests.js')
+    configurations.files.push('dist/DexieOpenedDatabasesAddon.ie11up.js')
+    configurations.files.push('test/tmp/tests.ie11up.js')
     config.set(configurations)
 }
